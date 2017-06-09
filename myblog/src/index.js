8441ae20d54698eb6f8c8617ef8e8dcb8e0bbf33
Ni Tongyang <ntyaiqingr@gmail.com>
2017-06-09 10:22:52 -0700

    combine with the react-bootstrap

--- myblog/src/index.js
+++ myblog/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
   return (
-    <div>Hi!</div>
+    <div>
+      <div>Hello world</div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
--- myblog/src/main.js
+++ myblog/src/main.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header';
+import Footer from './components/footer';
 
-
-const App = function() {
+const App = () => {
   return (
-    <div>Hi!</div>
+    <div>
+      <Header />
+    </div>
+    </div>
+      <Footer />
+    </div>
   );
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
